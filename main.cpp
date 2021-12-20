2ea6b18b46fa8d3abd784a9691847dea02b2c080
Mustafa <mkafridi073@gmail.com>
2021-12-20 12:25:21 +0500

    .

--- main.cpp
+++ main.cpp
@@ -1,19 +1,7 @@
-                                               //
-                                               // Created by mustafa on 12/6/21.
-                                               //
 #include "iostream"
 using namespace std;
 
 int main()
 {
-    int n;
-    cin>>n;
-    int a[n];
-    for(int i = 0; i<=n-1; i++)
-    {
-        cin>>a[i];
-    }
-    for(int i = n - 1;i>=0; i--)
-        cout<<a[i]<<" ";
-   return 0;
+
 }
